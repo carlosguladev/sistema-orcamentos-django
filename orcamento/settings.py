10c7bf890f57dcea7e6c785cacb6757fe035645b
Carlos Gula <carlosguladev@gmail.com>
2026-01-29 19:03:42 -0300

    Initial commit - Django project

--- orcamento/settings.py
+++ orcamento/settings.py
@@ -85,12 +85,20 @@ WSGI_APPLICATION = 'orcamento.wsgi.application'
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
 DATABASES = {
-    "default": dj_database_url.config(
-        default=os.environ.get("DATABASE_URL", "sqlite:///db.sqlite3"),
-        conn_max_age=600,
-        ssl_require=True
-    )
+  "default": {
+    "ENGINE": "django.db.backends.postgresql",
+    "NAME": os.environ.get("DB_NAME"),
+    "USER": os.environ.get("DB_USER"),
+    "PASSWORD": os.environ.get("DB_PASSWORD"),
+    "HOST": os.environ.get("DB_HOST"),
+    "PORT": os.environ.get("DB_PORT", "5432"),
+    "OPTIONS": {
+      "sslmode": os.environ.get("DB_SSLMODE", "require"),
+    },
+  }
 }
+
+
 
 
 
